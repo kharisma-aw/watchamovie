957a0caac7ae3f058b6dad6b056c2bfa269c557b
kharisma-aw <aw.kharisma@gmail.com>
2020-01-08 11:00:36 +0700

    create notification channel in app class

--- app/src/main/java/com/awkris/watchamovie/WatchAMovie.kt
+++ app/src/main/java/com/awkris/watchamovie/WatchAMovie.kt
@@ -1,10 +1,12 @@
 package com.awkris.watchamovie
 
 import android.app.Application
+import androidx.core.app.NotificationManagerCompat
 import com.awkris.watchamovie.di.component.ApplicationComponent
 import com.awkris.watchamovie.di.component.DaggerApplicationComponent
 import com.awkris.watchamovie.di.module.ApiModule
 import com.awkris.watchamovie.di.module.ApplicationModule
+import com.awkris.watchamovie.utils.NotificationUtils
 import com.facebook.stetho.Stetho
 import timber.log.Timber
 
@@ -18,6 +20,13 @@ class WatchAMovie : Application() {
         if (BuildConfig.BUILD_TYPE.contentEquals("debug")) {
             Stetho.initializeWithDefaults(this)
         }
+
+        NotificationUtils.createNotificationChannel(
+            this,
+            NotificationManagerCompat.IMPORTANCE_DEFAULT,
+            getString(R.string.app_name),
+            "Movie reminder channel"
+        )
     }
 
     private fun createAppComponent(): ApplicationComponent {
