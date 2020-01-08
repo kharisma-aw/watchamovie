d057f813af6554ff93aa2916861ae7b219ffc569
kharisma-aw <aw.kharisma@gmail.com>
2020-01-08 10:32:29 +0700

    modify applicationcomponent

--- app/src/main/java/com/awkris/watchamovie/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/awkris/watchamovie/di/component/ApplicationComponent.kt
@@ -2,23 +2,28 @@ package com.awkris.watchamovie.di.component
 
 import android.content.Context
 import com.awkris.watchamovie.data.repository.MovieDbRepository
-import com.awkris.watchamovie.data.room.MovieDatabase
 import com.awkris.watchamovie.di.ApplicationScope
 import com.awkris.watchamovie.di.module.ApiModule
 import com.awkris.watchamovie.di.module.ApplicationModule
+import com.awkris.watchamovie.di.module.MainActivityModule
+import com.awkris.watchamovie.presentation.main.MainActivity
 import com.awkris.watchamovie.presentation.moviedetail.MovieDetailActivity
-import com.awkris.watchamovie.presentation.nowplaying.NowPlayingActivity
+import com.awkris.watchamovie.presentation.nowplaying.NowPlayingDataSource
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 
 @ApplicationScope
 @Component(modules = [
+    AndroidInjectionModule::class,
     ApplicationModule::class,
-    ApiModule::class
+    ApiModule::class,
+    MainActivityModule::class
 ])
 interface ApplicationComponent {
-    fun applicationContext(): Context
+    fun inject(activity: MainActivity)
     fun inject(activity: MovieDetailActivity)
-    fun inject(activity: NowPlayingActivity)
-    fun movieDb(): MovieDatabase
+
+    fun applicationContext(): Context
+    fun nowPlayingDataSource(): NowPlayingDataSource
     fun repository(): MovieDbRepository
 }
